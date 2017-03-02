f419dffd258f5716c37a353b66e495211520a791
Devechha ramekar <devechha.ramekar@globallogic.com>
2017-03-02 17:00:09 +0530

    added service code

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: `<h1>Hello {{ name }}!</h1>`
+     templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
     name: string;
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,17 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 
+import { ParentComponent } from "./service/parent.component";
+import { CmpAComponent } from "./service/cmp-a.component";
+import { CmpBComponent } from "./service/cmp-b.component";
+
 @NgModule({
     imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent,
+        ParentComponent,
+        CmpAComponent,
+        CmpBComponent
+         ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
